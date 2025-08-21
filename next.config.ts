c60881d9a946ce3750ff80ea5211884dfdc72ae7
Kasturibangar2008 <kasturibangar2008@gmail.com>
2025-08-21 13:16:09 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+ enlist:{
+  ignoreDuringBuilds: true,
+ },
+ typescript:{
+  ignoreBuildErrors: true,
+ }
+};
+
+export default nextConfig;
